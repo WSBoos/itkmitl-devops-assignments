4636bc66bc27a678cfe027566d8ddaec9f3598b6
Warangkhana Sukpartcharoen <62070166@it.kmitl.ac.th>
2021-11-11 09:30:01 +0000

    add assignment09

---
+++ assignments/09-deploy-helm-mongodb-dev.sh
@@ -0,0 +1,11 @@
+# Create secret
+kubectl create secret generic bookinfo-dev-ratings-mongodb-secret \
+  --from-literal=mongodb-password=CHANGEME \
+  --from-literal=mongodb-root-password=CHANGEME
+# Create configmap
+kubectl create configmap bookinfo-dev-ratings-mongodb-initdb \
+  --from-file=databases/ratings_data.json \
+  --from-file=databases/script.sh
+# Deploy MongoDB with Helm Value
+helm install -f k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml \
+  bookinfo-dev-ratings-mongodb bitnami/mongodb --version 10.28.4
